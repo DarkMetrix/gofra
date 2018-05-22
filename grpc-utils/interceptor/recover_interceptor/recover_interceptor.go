a36ea96dc46eaa160b3aae23acf3702afa6619f4
techie <york528@yeah.net>
2018-05-22 18:30:28 +0800

    feat:Add log to recover intercepter when panic

--- grpc-utils/interceptor/recover_interceptor/recover_interceptor.go
+++ grpc-utils/interceptor/recover_interceptor/recover_interceptor.go
@@ -1,11 +1,24 @@
 package recover_interceptor
 
 import (
+	"runtime/debug"
+
+	log "github.com/cihub/seelog"
+
 	"google.golang.org/grpc"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 )
 
 func GetServerInterceptor() grpc.UnaryServerInterceptor {
-	return grpc_recovery.UnaryServerInterceptor()
+	recoverFunc := func (p interface{}) error {
+		log.Errorf("Got panic! error:%v, stack:%v", p, string(debug.Stack()))
+		return nil
+	}
+
+	opts := []grpc_recovery.Option{
+		grpc_recovery.WithRecoveryHandler(recoverFunc),
+	}
+
+	return grpc_recovery.UnaryServerInterceptor(opts...)
 }
