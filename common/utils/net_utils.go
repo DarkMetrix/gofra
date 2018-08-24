3391c87c26a5a8697e444262e135f18cb55601d1
TechieYork <york528@yeah.net>
2018-08-24 16:14:22 +0800

    feat:Add GetRealIPv4ByNetwork

--- common/utils/net_utils.go
+++ common/utils/net_utils.go
@@ -17,16 +17,27 @@ func GetRealAddrByNetwork(addr string) string {
 	name := parts[0]
 	port := parts[1]
 
-	i, err := net.InterfaceByName(name)
+	ip, err := GetRealIPv4ByNetwork(name)
 
 	if err != nil {
 		return addr
+	} else {
+		return fmt.Sprintf("%v:%v", ip, port)
+	}
+}
+
+//Utils to help get real ip using name like 'eth1'
+func GetRealIPv4ByNetwork(name string) (string, error) {
+	i, err := net.InterfaceByName(name)
+
+	if err != nil {
+		return "", err
 	}
 
 	addrs, err := i.Addrs()
 
 	if err != nil {
-		return addr
+		return "", err
 	}
 
 	for _, addr := range addrs {
@@ -34,10 +45,10 @@ func GetRealAddrByNetwork(addr string) string {
 
 		if ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				return fmt.Sprintf("%v:%v", ipnet.IP.String(), port)
+				return ipnet.IP.String(), nil
 			}
 		}
 	}
 
-	return addr
+	return "0.0.0.0", nil
 }
