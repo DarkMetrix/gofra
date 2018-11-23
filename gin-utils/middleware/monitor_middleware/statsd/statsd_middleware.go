304a2f5f17bc567a10747ecf3f61123306b1b0b2
TechieYork <york528@yeah.net>
2018-11-23 11:19:42 +0800

    fix:Panic if statsd is not init successfully

--- gin-utils/middleware/monitor_middleware/statsd/statsd_middleware.go
+++ gin-utils/middleware/monitor_middleware/statsd/statsd_middleware.go
@@ -10,21 +10,39 @@ import (
 
 func GetMiddleware() gin.HandlerFunc{
 	return func (ctx *gin.Context) {
-		//Before request
-		defer monitor.GetStatsd().NewTiming().Send(fmt.Sprintf("%v|Time,type=Server.Time", ctx.Request.RequestURI))
+		client := monitor.GetStatsd()
 
-		monitor.Increment(fmt.Sprintf("%v,type=Server.Total", ctx.Request.RequestURI))
+		if client != nil {
+			//Before request
+			defer monitor.GetStatsd().NewTiming().Send(fmt.Sprintf("%v|Time,type=Server.Time", ctx.Request.RequestURI))
 
-		//Switch to another middleware handler
-		ctx.Next()
+			monitor.Increment(fmt.Sprintf("%v,type=Server.Total", ctx.Request.RequestURI))
 
-		//After request
-		status := ctx.Writer.Status()
+			//Switch to another middleware handler
+			ctx.Next()
 
-		if status != 200 {
-			monitor.Increment(fmt.Sprintf("%v,type=Server.Fail", ctx.Request.RequestURI))
+			//After request
+			status := ctx.Writer.Status()
+
+			if status != 200 {
+				monitor.Increment(fmt.Sprintf("%v,type=Server.Fail", ctx.Request.RequestURI))
+			} else {
+				monitor.Increment(fmt.Sprintf("%v,type=Server.Success", ctx.Request.RequestURI))
+			}
 		} else {
-			monitor.Increment(fmt.Sprintf("%v,type=Server.Success", ctx.Request.RequestURI))
+			monitor.Increment(fmt.Sprintf("%v,type=Server.Total", ctx.Request.RequestURI))
+
+			//Switch to another middleware handler
+			ctx.Next()
+
+			//After request
+			status := ctx.Writer.Status()
+
+			if status != 200 {
+				monitor.Increment(fmt.Sprintf("%v,type=Server.Fail", ctx.Request.RequestURI))
+			} else {
+				monitor.Increment(fmt.Sprintf("%v,type=Server.Success", ctx.Request.RequestURI))
+			}
 		}
 	}
 }
