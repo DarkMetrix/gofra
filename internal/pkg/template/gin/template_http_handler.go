2f351b1ddfc87f0f4369311f8f5a3799c22e60e9
york <york528@yeah.net>
2020-03-10 21:44:43 +0800

    feat:Modify http handler template to remove some comments

--- internal/pkg/template/gin/template_http_handler.go
+++ internal/pkg/template/gin/template_http_handler.go
@@ -37,38 +37,7 @@ import (
 func {{.HandlerName}}(ctx *gin.Context) {
 	log.Tracef("====== {{.HandlerName}} start ======")
 
-	/*
-	// parse request
-	// TODO: Bind json to request
-	var req xxx
-
-	err := ctx.BindJSON(&req)
-
-	if err != nil {
-		log.Warnf("ctx.BindJSON failed! error:%v", err.Error())
-		ctx.AbortWithStatusJSON(520, gin.H{"ret":-1, "msg":"Bad json body!"})
-		return
-	}
-
-	// check params
-	// TODO: Check params
-	err = check{{.HandlerName}}Params(&req)
-
-	if err != nil {
-		log.Warnf("check{{.HandlerName}}Params failed! error:%v", err.Error())
-		ctx.AbortWithStatusJSON(520, gin.H{"ret":-1, "msg":fmt.Sprintf("Param invalid! error:%v", err.Error())})
-		return
-	}
-	*/
-
 	// reply success
 	ctx.JSON(200, gin.H{"ret":0, "msg":"success"})
 }
 
-/*
-// TODO: Implement check{{.HandlerName}}Params function
-func check{{.HandlerName}}Params(req *xxx) error {
-	return nil
-}
-*/
-`
